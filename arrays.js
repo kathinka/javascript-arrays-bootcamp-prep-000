0f2818605dd15323e9976dee95d0a879be9b37f3
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-01 22:31:27 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,33 @@
+var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var array = [];
+var array2 =[];
+var array3 = [];
+
+function addElementToBeginningOfArray(array,element){
+ var array2 = [element,...array];
+ return (array2);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return (array);
+}
+function destructivelyRemoveElementToBeginningOfArray(array, element){
+  array.shift();
+  return(array);
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return (array);
+}
+function destructivelyRemoveElementToEndOfArray(array, element){
+  array.pop();
+  return (array);
+}
+
+function addElementToEndOfArray(array, element){
+  array3 = [...array, element];
+  return (array3);
+}
